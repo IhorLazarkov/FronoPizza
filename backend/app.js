514c1c6746b749b14a248acb69927e9d3e14762a
ilazarkov <ilazarkov@gmail.com>
2025-04-25 20:23:13 -0500

    added migrtions, seeder for suer and router

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,59 @@
 const express = require('express');
-const app = express();
 const routes = require('./routes');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+
+const  { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed CSRF token
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+
+//Check if Sequelize Validation Errors
+app.use((err, _req, _res, next) => {
+    if(err instanceof ValidationError){
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Last line of defense
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 app.use(routes);
 
---
+++ backend/db/seeders/20250426004212-users.js
@@ -0,0 +1,49 @@
+'use strict';
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+const options = { tableName: "Users" };
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await User.bulkCreate([
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        firstname: "Firstname",
+        lastname: "Lastname",
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'fake@user.io',
+        username: 'Fakeuser1',
+        firstname: "Firstname1",
+        lastname: "Lastname1",
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(options, null, {});
+  }
+};
