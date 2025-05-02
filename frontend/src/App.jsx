b04033f654a429ffa50ac09678db7a445f7abf5f
Ihor Lazarkov <86078653+IhorLazarkov@users.noreply.github.com>
2025-05-02 18:22:33 -0500

    incorporated login/logoff functionality; splitted pages into components (#9)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,73 +1,28 @@
 import './App.css'
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { useState, useEffect } from 'react'
-import { login, logout } from './store/session'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import { MyCart } from './components/MyCart/MyCart'
 import OrderLivePage from './components/OrderLive/OrderLivePage'
-
-function Login() {
-  const dispatch = useDispatch()
-  const user = useSelector(state => state.user)
-
-  // UI state
-  const [email, setEmail] = useState("demo@user.io")
-  const [password, setPassword] = useState("password")
-  const [error, setError] = useState("")
-
-  const onSumbitLogin = (e) => {
-    e.preventDefault()
-    dispatch(login({ email, password }))
-      .then((res) => {
-        if (!res.ok) setError(res.title)
-        else setError("")
-      })
-  }
-
-  const onLogout = () => {
-    dispatch(logout())
-      .then((res) => {
-        if (!res.ok) {
-          setError(res.title)
-        }
-      })
-  }
-
-  useEffect(() => {
-    console.log({ user })
-  }, [user])
-
-  return (
-    <>
-      <div style={{ borderColor: "red", color: "red" }}>{error}</div>
-      <form onSubmit={onSumbitLogin}>
-        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        <input type="submit" value="Login" />
-        <input type="button" value="Logout" onClick={onLogout} />
-      </form>
-    </>
-  )
-}
+import LoginPage from './components/LoginPage'
 
 const router = createBrowserRouter(
   [
     {
-      path: "/",
-      element: <LandingPage />,
-    },
-    {
-      path: "login",
-      element: <Login />,
-    },
-    {
-      path: "mycart",
-      element: <MyCart />,
-    },
-    {
-      path: "orderlive/:id",
-      element: <OrderLivePage />,
+      element: <LoginPage />,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage />,
+        },
+        {
+          path: "mycart",
+          element: <MyCart />,
+        },
+        {
+          path: "orderlive/:id",
+          element: <OrderLivePage />,
+        }
+      ]
     }
   ]
 );
