c77001a9accb48d4277f5eef53c9acd081c85091
ilazarkov <ilazarkov@gmail.com>
2025-05-03 17:56:45 -0500

    added login of number of reviews with whether the user has review so he can add it if not

--- frontend/src/components/PizzaDetailsPage/PizzaDetailsPage.jsx
+++ frontend/src/components/PizzaDetailsPage/PizzaDetailsPage.jsx
@@ -1,7 +1,7 @@
 import "./PizzaDetails.css";
 import { AiOutlineLike } from "react-icons/ai";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPizzaDetails } from "../../store/pizzaDetails";
@@ -9,8 +9,11 @@ import { getPizzaDetails } from "../../store/pizzaDetails";
 export default function PizzaDetailsPage() {
     const { id } = useParams();
     const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
     const pizza = useSelector(state => state.pizzaDetails);
     const [pizzaState, setPizzaState] = useState(pizza);
+    const isReviewOwner = useRef(false);
+    const numReviews = useRef(0);
 
     const arrMonth = ["January", "February", "March", "April", "May", "June", "July",
         "August", "September", "October", "November", "December"];
@@ -24,7 +27,12 @@ export default function PizzaDetailsPage() {
 
     useEffect(() => {
         dispatch(getPizzaDetails(id))
-            .then(pizzaDetails => setPizzaState(pizzaDetails))
+            .then(pizzaDetails => {
+                const { id } = user;
+                isReviewOwner.current = pizzaDetails.Reviews.filter(({ User }) => User.id === id).length > 0
+                numReviews.current = pizzaDetails.Reviews.length
+                setPizzaState(pizzaDetails)
+            })
             .catch(error => console.error({ error }));
     }, [dispatch]);
 
@@ -35,7 +43,7 @@ export default function PizzaDetailsPage() {
                 <div className="info" >
                     <h1>{pizzaState.name}</h1>
                     <p>{pizzaState.description}</p>
-                    <div style={{fontSize:"1.5rem"}}>Cost: ${pizzaState.price}</div>
+                    <div style={{ fontSize: "1.5rem" }}>Cost: ${pizzaState.price}</div>
                 </div>
             </div>
             <section className="ingredients">
@@ -49,7 +57,12 @@ export default function PizzaDetailsPage() {
                 ))}
             </section>
             <section className="reviews">
-                <h3>Reviews:</h3>
+                {numReviews.current == 0
+                    ? <h3>There are no reviews yet</h3>
+                    : <h3>Reveiws: {numReviews.current}</h3>}
+                {!isReviewOwner.current && <button
+                    style={{ width: "max-content", padding: "10px", fontSize: "1rem" }}
+                    className="primary">Add Review</button>}
                 {pizzaState.Reviews.map(r => (
                     <div key={r.id} className="review">
                         <span>{printDate(r.updatedAt)}</span>
