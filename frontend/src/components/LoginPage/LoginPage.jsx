c6d952eb07416e520a1587bf8cd6a895a1b43fea
ilazarkov <ilazarkov@gmail.com>
2025-05-04 16:15:42 -0500

    added seperate components

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -8,22 +8,12 @@ import Navigation from "../Navigation/Navigation"
 export default function LoginPage() {
   const dispatch = useDispatch()
   const user = useSelector(state => state.user)
+  const [form, setForm] = useState(<LoginForm />)
 
   // UI state
-  const [email, setEmail] = useState("demo@user.io")
-  const [password, setPassword] = useState("password")
-  const [error, setError] = useState("")
   const [loggedInUser, setLoggedInUser] = useState(user)
 
-  const onSumbitLogin = (e) => {
-    e.preventDefault()
-    dispatch(login({ email, password }))
-      .then((res) => {
-        if (!res.ok) setError(res.title)
-      })
-  }
-
-  const onLogout = () => { dispatch(logout()) }
+  const onLogout = () => dispatch(logout())
 
   useEffect(() => {
     dispatch(restoreUser()).then((res) => {
@@ -39,15 +29,53 @@ export default function LoginPage() {
     <>
       {Object.values(loggedInUser).length
         ? <Navigation onLogout={onLogout} />
-        : <>
-          <form onSubmit={onSumbitLogin}>
-            <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            <input type="submit" value="Login" />
-          </form>
-          <div style={{ borderColor: "red", color: "red" }}>{error}</div>
-        </>
+        : <div id="login-page">
+          {form}
+        </div>
       }
     </>
+  )
+}
+
+function LoginForm() {
+
+  const dispatch = useDispatch()
+
+  // UI state
+  const [email, setEmail] = useState("demo@user.io")
+  const [password, setPassword] = useState("password")
+  const [error, setError] = useState("")
+
+  const onSumbitLogin = (e) => {
+    e.preventDefault()
+    dispatch(login({ email, password }))
+      .then((res) => {
+        if (!res.ok) setError(res.title)
+      })
+  }
+
+  return (
+    <div className="login-form">
+      <form onSubmit={onSumbitLogin}>
+        <img src="../../assets/logo.png" alt="logo" />
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button className="primary" type="submit">Login</button>
+        <button className="secondary">Signup</button>
+      </form>
+      <div style={{ borderColor: "red", color: "red" }}>{error}</div>
+    </div>
+  )
+}
+
+function SignUpForm() {
+  return (
+    <div className="signup-form">
+      <form>
+        <input type="text" />
+        <input type="password" />
+      </form>
+      <div style={{ borderColor: "red", color: "red" }}>{error}</div>
+    </div>
   )
 }
